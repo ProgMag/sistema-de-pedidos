2b20eae398a0cdcb40263bb6a7f6292381402fd1
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-08-19 18:20:47 -0300

    Adiciona import

--- src/br/com/pedidos/main/classe/Main.java
+++ src/br/com/pedidos/main/classe/Main.java
@@ -1,5 +1,8 @@
 package br.com.pedidos.main.classe;
 
+import br.com.pedidos.main.interfaces.ComDesconto;
+import br.com.pedidos.main.interfaces.ComTaxaEntrega;
+
 import java.util.ArrayList;
 
 public class Main {
