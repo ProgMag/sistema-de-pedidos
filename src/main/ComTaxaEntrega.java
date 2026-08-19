2b704687deefe8f1060a158b484342e0c3e52d53
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-08-19 16:28:34 -0300

    Adiciona método para calcular valor com a taxa

--- src/main/ComTaxaEntrega.java
+++ src/main/ComTaxaEntrega.java
@@ -3,4 +3,9 @@ package main;
 public interface ComTaxaEntrega {
 
     double calcularTaxaEntrega(double distanciaKm);
+
+    default double calcularValoComTaxa(double valorPedido, double distanciaKm) {
+        double taxaEntrega = calcularTaxaEntrega(distanciaKm);
+        return taxaEntrega + valorPedido;
+    }
 }
