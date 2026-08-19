c3bc485b714fce2d93eec79c639cd0fd0bcb0729
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-08-19 16:16:33 -0300

    Adiciona método para calcular valor com desconto

--- src/main/ComDesconto.java
+++ src/main/ComDesconto.java
@@ -3,4 +3,9 @@ package main;
 public interface ComDesconto {
 
     double calcularDesconto(double valorPedido);
+
+    default double calcularValorComDesconto(double valorPedido) {
+        double desconto = calcularDesconto(valorPedido);
+        return valorPedido - desconto;
+    }
 }
