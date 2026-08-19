95f6d197cb13f124aad7eb3cc47d82c1ab44da6a
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-08-19 16:39:17 -0300

    Adiciona construtor da classe

--- src/main/PedidoBalcao.java
+++ src/main/PedidoBalcao.java
@@ -2,6 +2,10 @@ package main;
 
 public class PedidoBalcao extends Pedido implements ComDesconto{
 
+    public PedidoBalcao(int idPedido, String nomeCliente, double valorPedido) {
+        super(idPedido, nomeCliente, valorPedido);
+    }
+
     @Override
     public double calcularDesconto(double valorPedido) {
         return valorPedido * 0.08;
