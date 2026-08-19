71ab51e7eaec4e537aae9795e9d9aef4764309cd
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-08-19 17:14:14 -0300

    Adiciona delivey na main

--- src/br/com/pedidos/main/classe/Main.java
+++ src/br/com/pedidos/main/classe/Main.java
@@ -3,5 +3,7 @@ package br.com.pedidos.main.classe;
 public class Main {
     static void main() {
 
+        PedidoDelivery delivery = new PedidoDelivery(2, "Guilherme", 83, 12);
+        delivery.exibirPedido();
     }
 }
