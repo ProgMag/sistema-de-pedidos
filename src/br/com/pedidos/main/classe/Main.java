addc9752d12217e0b400c68bb001fcdf492f1857
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-08-19 18:21:14 -0300

    Adiciona Array e instanceof na main

--- src/br/com/pedidos/main/classe/Main.java
+++ src/br/com/pedidos/main/classe/Main.java
@@ -8,7 +8,28 @@ import java.util.ArrayList;
 public class Main {
     static void main() {
 
-        PedidoDelivery delivery = new PedidoDelivery(2, "Guilherme", 83, 12);
-        delivery.exibirPedido();
+        ArrayList<Pedido> pedidos = new ArrayList<>();
+        pedidos.add(new PedidoBalcao(1, "Ricardo", 40.99));
+        pedidos.add(new PedidoDelivery(2, "Claudio", 32.89, 14));
+
+        for (Pedido pedido : pedidos) {
+            pedido.exibirPedido();
+
+            if (pedido instanceof ComDesconto desconto) {
+                System.out.printf("""
+                        Você teve um desconto de %.2f reais
+                        """, desconto.calcularDesconto(pedido.getValorPedido()));
+            } else {
+                System.out.print("Você não recebeu desconto\n");
+            }
+
+            if (pedido instanceof ComTaxaEntrega taxa) {
+                System.out.printf("""
+                        A taxa de entrega é %.2f
+                        """, taxa.calcularTaxaEntrega(pedido.getValorPedido()));
+            } else {
+                System.out.print("Seu pedido não se adequa a uma taxa de entrega\n");
+            }
+        }
     }
 }
