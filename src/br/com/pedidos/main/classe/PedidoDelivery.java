039fb59ccb46d20f746086bb3548e2966d0008fb
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-08-19 17:00:56 -0300

    Adiciona constructor

--- src/br/com/pedidos/main/classe/PedidoDelivery.java
+++ src/br/com/pedidos/main/classe/PedidoDelivery.java
@@ -5,4 +5,7 @@ import br.com.pedidos.main.interfaces.ComTaxaEntrega;
 
 public class PedidoDelivery extends Pedido implements ComDesconto, ComTaxaEntrega {
 
+    public PedidoDelivery(int idPedido, String nomeCliente, double valorPedido) {
+        super(idPedido, nomeCliente, valorPedido);
+    }
 }
