6c76ce7e059f1046ebb71977a77fa26cee40c4ed
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-08-20 16:40:46 -0300

    Adiciona valores finais para o desconto

--- src/br/com/pedidos/main/classe/Main.java
+++ src/br/com/pedidos/main/classe/Main.java
@@ -8,10 +8,9 @@ import java.util.ArrayList;
 public class Main {
     static void main() {
 
-        ComDesconto valorFinalComAniversario = valorPedido -> valorPedido * 0.2;
-        ComDesconto valorFinalComFidelidade = valorPedido -> valorPedido * 0.05;
+        ComDesconto descontoAniversario = valorPedido -> valorPedido * 0.2;
+        ComDesconto descontoFidelidade = valorPedido -> valorPedido * 0.05;
 
-        System.out.printf("");
 
         ArrayList<Pedido> pedidos = new ArrayList<>();
         pedidos.add(new PedidoBalcao(1, "Ricardo", 40.99));
@@ -20,10 +19,11 @@ public class Main {
         for (Pedido pedido : pedidos) {
             pedido.exibirPedido();
 
-            double valorFinalComAniversario = descontoAniversario.calcularDesconto(pedido.getValorPedido());
-            double valorFinalComFidelidade = descontoFidelidade.calcularDesconto(pedido.getValorPedido());
-
             if (pedido instanceof ComDesconto desconto) {
+
+                double valorFinalComAniversario = descontoAniversario.calcularDesconto(pedido.getValorPedido());
+                double valorFinalComFidelidade = descontoFidelidade.calcularDesconto(pedido.getValorPedido());
+
                 System.out.printf("""
                         Você teve um desconto de %.2f reais
                         Desconto de aniversário: R$ %.2f
